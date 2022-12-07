5bf317b091be83afd6ab799061adbc6db166e9c6
Yves-Henry Moraille <yves-henrymoraille@Yves-Henrys-Air.lan>
2022-12-07 08:12:10 -0500

    made some changes in the html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-var hour9El = $('#hour9');
- addClass('past');
-var hour10El= ('#hour10');
-addClass('present');
-var hour11El = ('#hour11');
-addClass('future');
 
-addClass('present')
+
+
+
 
 
 // the code isn'.cht run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
