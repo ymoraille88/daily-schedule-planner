235f4b864bdd7ca689b72f52111a0a18a3a07ee9
Yves-Henry Moraille <yves-henrymoraille@Yves-Henrys-Air.lan>
2022-12-08 23:39:05 -0500

    page is ready to deploy

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # daily-schedule-planner
+
+## project link
+
+[ProjectLink]()
+
+## Description
+
+This website you'll be able to save your daily activities and plan your day ahead.
+
+Each block has a color.  The gray represents the past event, The red represent to current Event and the Future is in Green.
+
+the app updates on a hourly basis.
+
+## Visuals
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+
 var currentDay = $('#currentDay')[0];
 currentDay.textContent =  dayjs().format('dddd, MMMM DD YYYY');
 
@@ -52,28 +53,28 @@ var fifteen = {
 };
 var timeEl = [nine, ten,eleven, twelve, thirteen, fourteen, fifteen];
 
-var dateFormat = dayjs().format('DD/MM/YYYY');
-var now = dayjs().format('DD/MM/YYYY');
+var dateFormat = dayjs().format('DD/MM/YYYY ');
+var now = dayjs().format('DD/MM/YYYY HH');
 
 for (i = 0; i < timeEl.length; i++) {
   dateFormatNow = dateFormat + timeEl[i].num;
   if (dayjs(now).isBefore(dateFormat)) {
-    timeEl[i].location.setAttribute('class', "row time-block ");
+    timeEl[i].location.setAttribute('class', "row time-block future ");
   };
   if (!dayjs(now).isBefore(dateFormat)) {
-    timeEl[i].location.setAttribute('class', "row time-block ");
+    timeEl[i].location.setAttribute('class', "row time-block  past");
   }
   if (dayjs(now).isBefore(dateFormat)) {
-    timeEl[i].location.setAttribute('class', "row time-block ");
+    timeEl[i].location.setAttribute('class', "row time-block present ");
   };
-console.log(dateFormat)
+
   nine.btn.addEventlistener("click", function () { saveInput(nine.location, 0) })
   ten.btn.addEventlistener("click", function () { saveInput(ten.location, 1) })
-  eleven.btn.addEventlistener("click", function () { saveInput(nine.location, 2) })
-  twelve.btn.addEventlistener("click", function () { saveInput(nine.location, 3) })
-  one.btn.addEventlistener("click", function () { saveInput(nine.location, 4) })
-  two.btn.addEventlistener("click", function () { saveInput(nine.location, 5) })
-  three.btn.addEventlistener("click", function () { saveInput(nine.location, 6) })
+  eleven.btn.addEventlistener("click", function () { saveInput(eleven.location, 2)})
+  twelve.btn.addEventlistener("click", function () { saveInput(twelve.location, 3)})
+  thirteen.btn.addEventlistener("click", function () { saveInput(thirteen.location, 4)})
+  fourteen.btn.addEventlistener("click", function () { saveInput(fourteen.location, 5)})
+  fifteen.btn.addEventlistener("click", function () { saveInput(fifteen.location, 6)})
 
 
 
@@ -87,27 +88,4 @@ console.log(dateFormat)
 
 
 
-
-
-// the code isn'.cht run until the browser has finished rendering all the elements
-// in the html.
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 
